e2f14557399671cc664bb9fddd414aa40c57a0f5
Billy Chan <waterfallbay@gmail.com>
2025-05-09 16:30:23 +1000

    testing new gen_availability_venue function 2

--- dispatch/gen_availability_dispatch.py
+++ dispatch/gen_availability_dispatch.py
@@ -2,22 +2,27 @@
 
 from datetime import datetime
 import pytz
-from tasks.availability import gen_availability
+from tasks.availability import gen_availability, gen_availability_venue
 
 def run_all_jobs():
     jobs = [
-        {"tenant_id": 1, "location_id": 1, "location_tz": "Australia/Sydney"},
-        {"tenant_id": 1, "location_id": 2, "location_tz": "Australia/Sydney"},
+        {"tenant_id": 2, "location_id": 34, "location_tz": "Australia/Sydney"},
+        {"tenant_id": 2, "location_id": 35, "location_tz": "Australia/Sydney"},
         # Add more as needed
     ]
 
+    
+
     for job in jobs:
-        now_local = datetime.now(pytz.timezone(job["location_tz"]))
-        if now_local.hour == 0:
-            print(f"[INFO] Generating availability for tenant {job['tenant_id']} location {job['location_id']} at {now_local}")
-            gen_availability.delay(job["tenant_id"], job["location_id"], job["location_tz"])
-        else:
-            print(f"[SKIP] It's not midnight in {job['location_tz']}")
+        print(f"[INFO] Generating availability for tenant {job['tenant_id']} location {job['location_id']} at {now_local}")
+        gen_availability_venue.delay(job["tenant_id"], job["location_id"], job["location_tz"])
+
+    #     now_local = datetime.now(pytz.timezone(job["location_tz"]))
+    #     if now_local.hour == 0:
+    #         print(f"[INFO] Generating availability for tenant {job['tenant_id']} location {job['location_id']} at {now_local}")
+    #         gen_availability.delay(job["tenant_id"], job["location_id"], job["location_tz"])
+    #     else:
+    #         print(f"[SKIP] It's not midnight in {job['location_tz']}")
 
 if __name__ == "__main__":
     run_all_jobs()
