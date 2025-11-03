7b4d774dca84801627ab95a4d2db65caae129592
Billy Chan <billy.chan@speako.ai>
2025-11-04 10:55:37 +1100

    restructured the entire project folders and files

--- test_email.py
+++
@@ -1,8 +0,0 @@
-from tasks.sms import send_email_confirmation_new_rest, send_email_confirmation_new, send_email_confirmation_mod_rest, send_email_confirmation_mod, send_email_confirmation_can_rest, send_email_confirmation_can
-
-if __name__ == "__main__":
-    result = send_email_confirmation_can(booking_id=10328)
-    if result:
-        print("✅ Task completed with result:")
-    else:
-        print("❌ No result returned")
